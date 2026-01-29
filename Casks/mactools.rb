5edafc1f200d9f9f0080f9e68d7aa9c01cf5da2c
lishugupta652 <lishugupta652@gmail.com>
2026-01-29 21:59:11 +0530

    first

--- Casks/mactools.rb
+++ Casks/mactools.rb
@@ -1,6 +1,6 @@
 cask "mactools" do
   version "1.0.0"
-  sha256 "REPLACE_WITH_SHA256"
+  sha256 "8e56ac0310281aeb45ae391caffdbc8fe4f57edb55052668b47decfdc4c7fe18"
 
   url "https://github.com/yourname/mactools/releases/download/v#{version}/MacTools-#{version}.zip"
   name "MacTools"
